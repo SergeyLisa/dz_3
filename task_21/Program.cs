0db1000cefe8acd9297f393dc0aa8d5af01ae5db
Sergey <Sergey@yqndex.ru>
2023-02-05 22:36:54 +0300

    all

---
+++ task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Введите координаты x1: ");
+double x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты y1: ");
+double y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты z1: ");
+double z1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координаты x2: ");
+double x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты y2: ");
+double y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты z2: ");
+double z2 = int.Parse(Console.ReadLine()!);
+double result=Math.Sqrt((Math.Pow((x1-x2),2)+Math.Pow((y1 - y2),2)+Math.Pow((z1 - z2),2)));
+Console.WriteLine($"Растояние между заданными точками в 3D пространстве: {result:f2}");
---
+++ task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int f, count = 1;
+while (count <= a)
+{
+  f = count * count * count;
+  {
+    Console.Write($" {f}.");
+  }
+  count++;
+}
